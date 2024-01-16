12603da3a93458773f6dfbbd2fa4d706e001b184
arthurs14 <arthurcs426@gmail.com>
2024-01-16 14:09:36 -0800

    create connect and disconnect functions for database

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,1 +1,30 @@
 /* Connect to database */
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import ora from "ora";
+import chalk from "chalk";
+
+dotenv.config();
+
+export async function connectDB() {
+  try {
+    const spinner = ora("Connecting to the database...").start();
+    await mongoose.connect(process.env.MONGO_URI);
+    spinner.stop();
+    console.log(chalk.greenBright("Successfully connected to database!"));
+  } catch (e) {
+    console.log(chalk.redBright("Error:"), e);
+    process.exit(1);
+  }
+}
+
+export async function disconnectDB() {
+  try {
+    await mongoose.disconnect();
+    console.log(chalk.greenBright("Disconnected from the database."));
+  } catch (e) {
+    console.log(chalk.redBright("Error:"), e);
+    process.exit(1);
+  }
+}
