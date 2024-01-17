d83c5052f80a34f0c39c5556d88d0239fd489ebb
arthurs14 <arthurcs426@gmail.com>
2024-01-17 13:02:39 -0800

    created the input questions to get user input of tasks

--- commands/addTask.js
+++ commands/addTask.js
@@ -1,1 +1,23 @@
 /* Create a New Todo */
+
+import inquirer from "inquirer";
+import { connectDB, disconnectDB } from "../db/connectDb.js";
+import Todos from "../schema/TodoSchema.js";
+import ora from "ora";
+import chalk from "chalk";
+
+async function input() {
+  const answers = await inquirer.prompt([
+    { name: "name", message: "Enter name of the task:", type: "input" },
+    {
+      name: "detail",
+      message: "Enter the details of the task:",
+      type: "input",
+    },
+  ]);
+
+  return answers;
+}
+
+// const output = await input();
+// console.log(output);
