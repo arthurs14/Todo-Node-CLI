cc8584766144e6f111d8548c43d48bb744a70158
arthurs14 <arthurcs426@gmail.com>
2024-01-17 12:55:44 -0800

    created todos model and schema

--- schema/TodoSchema.js
+++ schema/TodoSchema.js
@@ -1,1 +1,43 @@
 /* Mongoose Schema and Model */
+
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+
+const TodoSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    detail: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["completed", "pending"],
+      default: "pending",
+      trim: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      defautl: "code",
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// pre-save hook => runs everytime beore a task gets saved
+TodoSchema.pre("save", (next) => {
+  this.code = nanoid(10);
+  next();
+});
+
+// Todos Model and export
+const Todos = mongoose.model("Todos", TodoSchema);
+export default Todos;
