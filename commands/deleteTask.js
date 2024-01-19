715c976d28d27fc4cd43ce712b3c7dfbdd47228f
arthurs14 <arthurcs426@gmail.com>
2024-01-18 16:04:46 -0800

    created function that will get the code from user input

--- commands/deleteTask.js
+++ commands/deleteTask.js
@@ -1,1 +1,23 @@
 /* Delete a Todo */
+
+import inquirer from "inquirer";
+import Todos from "../schema/TodoSchema.js";
+import { connectDB, disconnectDB } from "../db/connectDb.js";
+import ora from "ora";
+import chalk from "chalk";
+
+export async function getTaskCode() {
+  try {
+    // prompt the user to enter the todo code
+    const answers = await inquirer.prompt([
+      { name: "code", message: "Enter the code of the todo:", type: "input" },
+    ]);
+
+    // trim user response
+    answers.code = answers.code.trim();
+
+    return answers;
+  } catch (e) {
+    console.log("Something went wrong...\n", e);
+  }
+}
