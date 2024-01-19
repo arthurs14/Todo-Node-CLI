64cad98b709ca241260bcfd27fe043b58eed8071
arthurs14 <arthurcs426@gmail.com>
2024-01-19 13:03:03 -0800

    created function that will gather user input to update the todo

--- commands/updateTask.js
+++ commands/updateTask.js
@@ -1,1 +1,39 @@
 /* Update a Todo */
+
+import { connectDB, disconnectDB } from "../db/connectDb.js";
+import { getTaskCode } from "./deleteTask.js";
+import inquirer from "inquirer";
+import Todos from "../schema/TodoSchema.js";
+import ora from "ora";
+import chalk from "chalk";
+
+async function askUpdateQ(todo) {
+  try {
+    // prompt the user to update the todo data
+    const update = await inquirer.prompt([
+      {
+        name: "name",
+        message: "Update the name?",
+        type: "input",
+        default: todo.name,
+      },
+      {
+        name: "detail",
+        message: "Update the Description?",
+        type: "input",
+        default: todo.detail,
+      },
+      {
+        name: "status",
+        message: "Update the status",
+        type: "list",
+        choices: ["pending", "completed"],
+        default: todo.status,
+      },
+    ]);
+
+    return update;
+  } catch (e) {
+    console.log("Something went wrong...\n", e);
+  }
+}
